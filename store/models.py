af2640852a29eacedba12df58fac1ae07c1dc268
Alicefangxin <98005984+Alicefangxin@users.noreply.github.com>
2023-02-12 10:49:11 -0800

    ecommerce initial commit

---
+++ store/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    date_placed = models.DateTimeField(auto_now_add=True)
+    date_shipped = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Order for {self.customer.first_name} {self.customer.last_name} - {self.product.name}"
---
+++ store/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+from .models import Product, Category, Customer, Order
+from django.http import JsonResponse
+
+def create_order(request):
+    customer_id = request.POST.get('customer_id')
+    product_id = request.POST.get('product_id')
+    customer = Customer.objects.get(id=customer_id)
+    product = Product.objects.get(id=product_id)
+    order = Order.objects.create(customer=customer, product=product)
+    return JsonResponse({'message': 'Order created successfully!'})
+
+def shopping_cart(request, customer_id):
+    customer = Customer.objects.get(id=customer_id)
+    orders = Order.objects.filter(customer=customer)
+    return render(request, 'store/shopping_cart.html', {'orders': orders})
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'store/product_list.html', {'products': products})
+
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'store/category_list.html', {'categories': categories})
+
+
+def customer_list(request):
+    customers = Customer.objects.all()
+    return render(request, 'store/customer_list.html', {'customers': customers})
+
+
+def order_list(request):
+    orders = Order.objects.all()
+    return render(request, 'store/order_list.html', {'orders': orders})
+
+
+def homepage(request, customer_id):
+    customer = Customer.objects.get(id=customer_id)
+    products = Product.objects.all()
+    return render(request, 'store/homepage.html', {'customer': customer, 'products': products})
+
+
+def product_detail(request, id):
+    product = Product.objects.get(id=id)
+    return render(request, 'store/product_detail.html', {'product': product})
+
+
+def order_form(request, customer_id, product_id):
+    customer = Customer.objects.get(id=customer_id)
+    product = Product.objects.get(id=product_id)
+    order = Order(customer=customer, product=product)
+    order.save()
+    return render(request, 'store/order_form.html', {'order': order})
+
+
+def submit_order(request, customer_id, product_id):
+    customer = Customer.objects.get(id=customer_id)
+    product = Product.objects.get(id=product_id)
+    order = Order.objects.create(customer=customer, product=product)
+    return render(request, 'store/submit_order.html', {'order': order})
+
